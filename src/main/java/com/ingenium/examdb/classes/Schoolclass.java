d9ff09c4579f9df72c2b5ae702018ee65bd27e9a
Guggi <michael-guggenberger@gmx.at>
2023-03-17 20:57:59 +0100

    joinColumns_4

--- src/main/java/com/ingenium/examdb/classes/Schoolclass.java
+++ src/main/java/com/ingenium/examdb/classes/Schoolclass.java
@@ -1,5 +1,6 @@
 package com.ingenium.examdb.classes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,9 +20,11 @@ public class Schoolclass {
     @Id
     @Column(name = "class_id")
     private long id;
+    @Column(name = "classname", length = 10)
     private String name;
 
     @OneToMany(mappedBy = "schoolclass")
+    @JsonIgnore //Vermeidung des Zirkelbezugs beim Umwandlung des Json
     private List<Student> studentList;
 
 }
